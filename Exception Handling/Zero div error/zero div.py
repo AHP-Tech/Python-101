83945508277783372f7ae820b43793d3bf9fde7a
poojitha1713 <43715183+poojitha1713@users.noreply.github.com>
2020-06-06 00:26:51 +0530

    Add files via upload

---
+++ Exception Handling/Zero div error/zero div.py
@@ -0,0 +1,6 @@
+try:
+    1 / 0
+except ZeroDivisionError:
+    print("You cannot divide by zero!")
+
+# You cannot divide by zero!
---
+++ Exception Handling/index and key.py
@@ -0,0 +1,6 @@
+my_dict = {"a":1, "b":2, "c":3}
+
+try:
+    value = my_dict["d"]
+except (IndexError, KeyError):
+    print("An IndexError or KeyError occurred!")
