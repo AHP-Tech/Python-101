0ac2756d18f11b07b9fec9895c2d9bc55812485c
poojitha1713 <43715183+poojitha1713@users.noreply.github.com>
2020-06-06 00:26:14 +0530

    Add files via upload

---
+++ Functions/defining/def.py
@@ -0,0 +1,3 @@
+def a_function():
+    print("You just created a function!")
+a_function()
---
+++ Functions/defining/stub.py
@@ -0,0 +1,5 @@
+# when you are writing out some code,
+# you just want to write the function definitions without putting any code in them.
+
+def empty_function():
+    pass
